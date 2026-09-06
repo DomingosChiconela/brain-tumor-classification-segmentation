6b0e3e0e33f7f57fd3a81dfa1db036904f5d5879
Domingos chiconela <azevedodomingos78@gmail.com>
2026-09-06 18:52:21 +0200

    feat(segmentation):  add

--- src/utils/load_datasets.py
+++ src/utils/load_datasets.py
@@ -15,3 +15,11 @@ def load_classification_dataset(data_path=None):
         ds["y_test"],
         ds["class_names"]
     )
+    
+    
+    
+    
+def load_segmentation_dataset():
+    npz_path = build_path("dataset", "segmentation", "brain_tumor_segmentation_dataset.npz")
+    data = np.load(npz_path, allow_pickle=True)
+    return data["X_train"], data["Y_train"], data["X_test"], data["Y_test"], data["class_names"]
